a7f24f0e490098583270247e54b5e8be114ddf08
Charles H Martin <charlesmartin14@gmail.com>
2018-09-14 09:12:56 -0700

    allennlp readme and scripts

---
+++ allennlp/run_analyzers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+allennlp analyze https://s3-us-west-2.amazonaws.com/allennlp/models/ner-model-2018.04.26.tar.gz > mac.dat
+allennlp analyze https://s3-us-west-2.amazonaws.com/allennlp/models/elmo-constituency-parser-2018.03.14.tar.gz > cp.dat
+allennlp analyze https://s3-us-west-2.amazonaws.com/allennlp/models/decomposable-attention-elmo-2018.02.19.tar.gza > elmo.dat
+allennlp analyze https://s3-us-west-2.amazonaws.com/allennlp/models/srl-model-2018.05.25.tar.gz > srl.dat
+allennlp analyze https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz > coref.dat
+allennlp analyze https://s3-us-west-2.amazonaws.com/allennlp/models/ner-model-2018.04.26.tar.gz > ner.dat
+
